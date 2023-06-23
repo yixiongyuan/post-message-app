dd2b5d07be0a60eacf1fd12bef666e0b843f1417
yixiongyuan <zachyyuan@gmail.com>
2023-06-23 12:19:37 -0400

    function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatInputModule} from '@angular/material/input';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatExpansionModule} from '@angular/material/expansion';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
-import { PostListComponent } from './posts/post-list/post-list.component';
-import { PostsService } from './posts/posts.service';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthInterceptor } from './auth/auth-interceptor';
+import { ErrorInterceptor } from './error-interceptor';
+import { ErrorComponent } from './error/error.component';
+import { AngularMaterialModule } from './angular-material.module';
+import { PostsModule } from './posts/posts.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent,
     HeaderComponent,
-    PostListComponent
+    ErrorComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatExpansionModule
+    HttpClientModule,
+    AngularMaterialModule,
+    PostsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {provide:HTTP_INTERCEPTORS,useClass:AuthInterceptor,multi:true},
+    {provide:HTTP_INTERCEPTORS,useClass:ErrorInterceptor,multi:true},
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,7 +1,12 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { Post } from "../post.model";
-import { NgForm } from "@angular/forms";
+import { FormControl, FormGroup, NgForm ,Validators} from "@angular/forms";
+import {ActivatedRoute, ParamMap} from "@angular/router";
 import { PostsService } from "../posts.service";
+import { mimeType } from "./mime-type.validator";
+import { Subscription } from "rxjs";
+import { AuthService } from "src/app/auth/auth.service";
+
 
 @Component({
   selector:'app-post-create',
@@ -9,20 +14,106 @@ import { PostsService } from "../posts.service";
   styleUrls:['./post-create.component.css']
 })
 
-export class PostCreateComponent{
+export class PostCreateComponent implements OnInit,OnDestroy {
   enteredTitle='';
   enteredContent = '';
+  post:Post;
+  isLoading = false;
+  form:FormGroup;
+  imagePreview:string;
+  private mode = 'create';
+  private postId :string;
+  private authStatusSub:Subscription;
 
-  constructor(public postsService: PostsService) {}
+
+  constructor(
+    public postsService: PostsService,
+    public route:ActivatedRoute,
+    private authService:AuthService
+    ) {}
 
 
-  onAddPost(form:NgForm){
+  ngOnInit() {
+    this.authStatusSub = this.authService.getAuthStatusListener().subscribe(
+      authStatus => {
+        this.isLoading = false;
+      }
+    );
+    this.form = new FormGroup({
+      'title': new FormControl(null,{
+        validators:[Validators.required,Validators.minLength(3)]
+      }),
+      'content':new FormControl(null,{validators:[Validators.required]}),
+      'image':new FormControl(null,{
+        validators:[Validators.required],
+        asyncValidators:[mimeType]
+      })
 
-    if(form.invalid){
+    });
+
+    this.route.paramMap.subscribe((paramMap:ParamMap) =>{
+      if(paramMap.has('postId')){
+
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        this.isLoading = true;
+
+        this.postsService.getPost(this.postId).subscribe(postData => {
+
+          this.isLoading = false;
+          this.post = {
+            id:postData._id,
+            title:postData.title,
+            content:postData.content,
+            imagePath:postData.imagePath,
+            creator:postData.creator,
+          };
+          this.form.setValue({
+            'title':this.post.title,
+            'content':this.post.content,
+            image:this.post.imagePath
+          });
+        });
+      } else {
+        this.mode = 'create';
+        this.postId = null;
+      }
+    });
+  }
+
+  onImagePicked(event:Event){
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image:file});
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () =>{
+      this.imagePreview = reader.result as string;
+    }
+    reader.readAsDataURL(file);
+  }
+
+  onSavePost(){
+
+    if(this.form.invalid){
       return;
     }
 
-    this.postsService.addPost(form.value.title,form.value.content);
-    form.resetForm();
+    this.isLoading = true;
+    if(this.mode === 'create'){
+      this.postsService.addPost(this.form.value.title,this.form.value.content,this.form.value.image);
+    } else {
+      this.postsService.updatePost(
+        this.postId,
+        this.form.value.title,
+        this.form.value.content,
+        this.form.value.image
+        )
+    }
+
+    this.form.reset();
+  }
+
+  ngOnDestroy() {
+    this.authStatusSub.unsubscribe();
   }
 }
